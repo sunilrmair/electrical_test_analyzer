b14bdfa81024f8de2ad27edc9fd0b3a6b539f5fb
Sunil Mair <smair@mit.edu>
2026-07-07 10:22:25 -0700

    Exposed EISFitter and EISFitTracer

--- electrical_test_analyzer/test_analyzers/__init__.py
+++ electrical_test_analyzer/test_analyzers/__init__.py
@@ -1,6 +1,7 @@
 from .raw_data_loader import RawDataLoader
 from .gitt_analyzer import GITTAnalyzer
 from .eis_analyzer import EISBasicAnalyzer, EISLoader
+from .eis_fitter import EISFitter, EISFitTracer
 from .metric_analyzer import EnergyPowerAnalyzer, MetricAnalyzer
 from .lifetime_analyzer import LifetimeAnalyzer
 from .polarization_analyzer import PolarizationAnalyzer
